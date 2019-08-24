4802f199e1a347b02d9984adbe24a7a22362aff0
Noordeen Malango <noordinmalango@gmail.com>
2019-08-25 01:33:55 +0200

    regex code

--- index.py
+++ index.py
@@ -7,3 +7,10 @@ Created on Sun Aug 25 01:21:07 2019
 #import necessary libraries for re and regex
 import re
 import regex
+
+email_validator = re.compile('[a-z]+')
+print(email_validator.match('schoolofaiBellevue'))
+
+#lets try to twist the above code
+email_validator = re.compile('[a-z]')
+print(email_validator.match('schoolofaiBellevue'))
